ef98580c014e52832e59175f660ed06707b6c0d9
Mark Fernando <mark93lk@gmail.com>
2026-06-14 00:27:00 +0300

    working on uart_write

--- uart.c
+++ uart.c
@@ -1,3 +1,33 @@
+/*
+This code is for Uart communication on BeagleBone Black (AM335x).
+I'm not responsible for the outcome of this code. It is for educational purposes only.
+Reffer Texas Instruments data sheet: https://www.ti.com/lit/ug/spruh73q/spruh73q.pdf
+*/
+
+
+/*
+MIT License
+
+Copyright (c) 2026 Mark Fernando
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+software and associated documentation files (the “Software”), to deal in the Software 
+without restriction, including without limitation the rights to use, copy, modify, 
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
+permit persons to whom the Software is furnished to do so, subject to the following 
+conditions:
+
+The above copyright notice and this permission notice shall be included in all copies 
+or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include "uart.h"
 
 #define UART0_BASE 0x44E090000
@@ -6,7 +36,6 @@
 #define UART0_DLH (UART0_BASE + 0x04) // when DLAB=1
 #define UART0_LCR (UART0_BASE + 0x0C)
 #define UART0_LSR (UART0_BASE + 0x14)
-
 
 void uart_init(){
     // need to use volatile. So, the compiler always reads/writes to the memory address, instead of optimizing it away.
@@ -35,16 +64,34 @@ void uart_init(){
 void uart_read(char *buf, int len){
     // refer Table 4-239 
     // LSR: Line Status Register    THR(Transmit Holding Register)  RHR(Receive Holding Register)
-    // If LSR() bit 5 is 1, THR is empty, we can write to it. If LSR bit 5 is 0, data is ready to be read from RHR..
-    *(volatile uint32_t *)(UART0_BASE + 0x14); // check if data is ready to be read (LSR bit 0)
+    
     // above line is incomplete.
 }
 
 void uart_write(char *str){
+    // refer Table 4-239
+    /*
+    Check the LSR bit 5
+    In non-FIFO mode:
+    0 Transmitter holding register (THR) is not empty. THR has been loaded by the CPU.
+    1 Transmitter holding register (THR) is empty (ready to accept a new character). The content of THR has been transferred to the transmitter shift register (TSR).
+    */
+    // If LSR() bit 5 is 1, THR is empty, we can write to it. If LSR bit 5 is 0, data is ready to be read from RHR..
+    // LSR & (1<<5)
+    // 0000 0001 <- shift left 5 times
+    // 0010 0000 = 0x20
+    // LSR & 0x20 (AND)
+    if (*(volatile uint32_t *)(UART0_BASE + 0x14) & 0x20) { 
+        // write to THR
+        *(volatile uint32_t *)(UART0_BASE + 0x00) = *str; // write the character to THR
+        
+    }
 
 }
  
-void mprintf(const char *format, ...){
+void mprintf(const char *str){
+    // void mprintf(const char *format, ...) if we use this we can use it like printf.
+    // With my current implementation, we can only print string.
     // check the LSR bit 5 to see if THR is empty. If it is empty then write the character to THR. 
 
 }
